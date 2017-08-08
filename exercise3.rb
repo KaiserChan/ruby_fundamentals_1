f7638b691ac122b887a4a5bd6d3704fc72965398
KaiserChan <chankaiserkk@gmail.com>
2017-08-08 15:11:53 -0400

    Updated exercise3.rb: Completed with question asking for age and birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,15 @@
 puts "Hello there, what is your name?"
+
+user_name = gets.chomp
+
+puts "Pleasure, #{user_name}"
+
+puts "So #{user_name}, how old are you?"
+
+user_age = gets.chomp.to_i
+
+current_year = 2017
+
+# birth_year = current_year - user_age
+
+puts "Ah! So you are #{user_age} years old, you were born in #{current_year - user_age}, correct?"
